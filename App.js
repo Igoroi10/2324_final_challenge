f5b7c02f56664c26fbd5fa71661aa91cd7866981
Etxebe <mikelgartzia21@gmail.com>
2024-02-05 13:14:03 +0100

    A5T-22[NEW] creado boton para salir y datos de usuario

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-
 import React, { useState } from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
 import Profile from './screens/Profile';
 
 const AppScreen = () => {
@@ -9,22 +8,46 @@ const AppScreen = () => {
   const user = {
     name: 'PATXI',
     email: 'aeg@gmail.com',
+    imageUri: 'https://lh3.googleusercontent.com/a/ACg8ocICfs24HN3aXJKBCUbfjW9RL4yZTnIkw7icAS0wMiPf7w=s96-c',
+    role: 'Acolite',
+    inventory: ["uno", "dos"],
+    changeStats: [1, 2, 3, 4],
+    changeMaxStats: [5, 6, 7, 8],
+    diseases: [0]
   };
 
-  const toggleProfile = () => {
+  const goToProfile = () => {
     setShowProfile(!showProfile);
+  };
+
+  const goBack = () => {
+    setShowProfile(false);
   };
 
   return (
     <View>
       <Text>FINAL CHALLENGE</Text>
       {!showProfile ? (
-        <Button title="Ir a Perfil" onPress={toggleProfile} />
+        <Button title="Ir a Perfil" onPress={goToProfile} />
       ) : (
-        <Profile user={user} />
+        <Profile user={user} goBack={goBack} />
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+});
+
 
 export default AppScreen;
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,12 +1,54 @@
-// Profile.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity , Button} from 'react-native';
 
-const Profile = ({ user }) => {
+
+const Profile = ({ user, goBack }) => {
   return (
     <View>
+      <Image
+        source={{ uri: user.imageUri }}
+        style={{ width: 100, height: 100, borderRadius: 50 }}
+      />
       <Text>Name: {user.name}</Text>
       <Text>Email: {user.email}</Text>
+  
+      <Text>Role: {user.role}</Text>
+
+      <Text>Inventory:</Text>
+      <View>
+        {user.inventory.map((item, index) => (
+          <Text key={index}>{item}</Text>
+        ))}
+      </View>
+
+      <Text>Change Stats:</Text>
+      <View>
+        {Object.keys(user.changeStats).map((key, index) => (
+          <Text key={index}>
+            {user.changeStats[key]}
+          </Text>
+        ))}
+      </View>
+
+      <Text>Change Max Stats:</Text>
+      <View>
+        {Object.keys(user.changeMaxStats).map((key, index) => (
+          <Text key={index}>
+            {user.changeMaxStats[key]}
+          </Text>
+        ))}
+      </View>
+
+      <Text>Diseases:</Text>
+      <View>
+        {Object.keys(user.diseases).map((key, index) => (
+          <Text key={index}>
+            {user.diseases[key]}
+          </Text>
+        ))}
+      </View>
+
+      <Button title="EXIT" onPress={goBack} />
     </View>
   );
 };
