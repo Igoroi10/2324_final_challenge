08a7692743ba8a52df3e2f2f5dfa2e350a92d680
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2024-02-07 12:32:18 +0100

    A5T-25 [NEW] Constants file created and globalStateSchema created

---
+++ src/helpers/Constants.js
@@ -0,0 +1,37 @@
+export const globalStateSchema = {
+
+    user:{
+        name: "",
+        email: "",
+        towerAccess: false,
+        rol: "",
+        inventory: [],
+        characterStats: {
+            hp: 100,
+            strength: 0,
+            agility: 0,
+            intelligence: 0
+        },
+        characterMaxStats: {
+            maxHp: 100,
+            maxStrength: 0,
+            maxAgility: 0,
+            maxIntelligence: 0
+        },
+        diseases: {
+            rotting_plague: false,
+            epic_weakness: false,
+            marrow_apocalypse: false,
+            ethazium: false,
+        },
+        imgURL : "",
+    },
+
+    userList: [],
+
+    battleStart:false,
+
+    initiative: [],
+    
+    currentTurn: "",
+}
