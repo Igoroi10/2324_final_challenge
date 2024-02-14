8ec2b69cd5f779897fe6edd82b6e07ec26ae2669
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-02-14 10:39:37 +0100

    [A5T-48][NEW] Firebase para iOS implementado correctamente

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -94,7 +94,7 @@ async function onGoogleButtonPress() {
 
     console.log("********************token****************************")
     console.log(idTokenResult.token);
-    const URL = "http://192.168.1.164:5000/api/users/token"
+    const URL = "http://192.168.1.166:5001/api/users/token"
     try{
       const decodedUser = await axios.post(URL, { idToken: idTokenResult.token });
       console.log(decodedUser.data)
