74bc2e5d8227c7b2693d3234e6faf1811e4fad2b
Etxebe <mikelgartzia21@gmail.com>
2024-02-19 13:45:06 +0100

    [UPDATE] boton log out programado

--- screens/ProfileAcolyte.js
+++ screens/ProfileAcolyte.js
@@ -3,13 +3,17 @@ import { Image, StyleSheet,Text } from 'react-native';
 import styled from 'styled-components/native';
 import { Context } from '../src/helpers/Context';
 import socket from '../helpers/socket';
-
-
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import LoginModal from '../src/components/LoginModal';
 
 const ProfileAcolyte = () => {
   const [showProfile, setShowProfile] = useState(true);
   const { globalState, globalStateHandler } = useContext(Context);
   const [blockButtonSocket, setBlockButtonSocket] = useState(false)
+  const [userRole, setUserRole] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isLoginModalVisible, setLoginModalVisible] = useState(false);
   useEffect(() => {
 
     console.log('userglobal', globalState);
@@ -33,70 +37,83 @@ const ProfileAcolyte = () => {
   const disableButton = ()=> {
     setBlockButtonSocket(true)
   }
+
+  async function onSignOutButtonPress() {
+    try {
+      await GoogleSignin.revokeAccess(); // Revoca el acceso de Google
+      await GoogleSignin.signOut(); // Cierra sesión de Google
+      await auth().signOut(); // Cierra sesión de Firebase (si estás utilizando Firebase)
+      setUserRole(null); // Actualiza el estado del usuario autenticado
+      setIsAuthenticated(false);
+      setLoginModalVisible(true);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   
   return (
     <View>
-  {globalState.user.name !== "" ? (
-    <View>
-    <Container>
-      <ProfileText>ACOLYTE</ProfileText>
-      <Image
-          source={{ uri: globalState.user.imgURL }}
-          style={{ width: 100, height: 100, borderRadius: 50 }}
-        />
-      <ProfileText>Name: {globalState.user.name}</ProfileText>
-      <ProfileText>Rol: {globalState.user.rol}</ProfileText>
-
-      <ProfileText>Inventory:</ProfileText>
-      <View style={styles.inlineContainer}>
-        {Object.keys(globalState.user.inventory).map((key, index) => (
-          <ProfileText key={index}>{globalState.inventory[key]}</ProfileText>
-        ))}
-      </View>
-
-    {/* Que es change stats? no existe ningún array en el globalStateSchema de changeStats */}
-      {/* <ProfileText>Change Stats:</ProfileText>
-      <View style={styles.inlineContainer}>
-        {Object.keys(globalState.changeStats).map((key, index) => (
-          <ProfileText key={index}>{globalState.changeStats[key]}</ProfileText>
-        ))}
-      </View> */}
-
-      <ProfileText>Diseases:</ProfileText>
-      <View style={styles.inlineContainer}>
-        {Object.keys(globalState.user.diseases).map((key, index) => (
-          <ProfileText key={index}>{globalState.user.diseases[key]}</ProfileText>
-        ))}
-      </View>
-
-      <ButtonContainer >
-        <CustomButton disabled={blockButtonSocket} onPress={()=>{
-          disableButton();
-          socket.emit("test_broadcast", globalState.user.name)
-          setBlockButtonSocket(false)
-        }} >
-          <ButtonText>envio de socket</ButtonText>
-        </CustomButton>
-        <CustomButton onPress={goBack}>
-          <ButtonText>Attack 2</ButtonText>
-        </CustomButton>
-        <CustomButton onPress={goBack}>
-          <ButtonText>Reset</ButtonText>
-        </CustomButton>
-      </ButtonContainer>
-
-      <SignOutButton onPress={goBack}>
-        <ButtonText>Sign Out</ButtonText>
-      </SignOutButton>
-    </Container>
+      {isAuthenticated ? (
+       
+        <View>
+          {globalState.user.name !== "" ? (
+            <Container>
+              <ProfileText>ACOLYTE</ProfileText>
+              <Image
+                source={{ uri: globalState.user.imgURL }}
+                style={{ width: 100, height: 100, borderRadius: 50 }}
+              />
+              <ProfileText>Name: {globalState.user.name}</ProfileText>
+              <ProfileText>Rol: {globalState.user.rol}</ProfileText>
+  
+              <ProfileText>Inventory:</ProfileText>
+              <View style={styles.inlineContainer}>
+                {Object.keys(globalState.user.inventory).map((key, index) => (
+                  <ProfileText key={index}>{globalState.inventory[key]}</ProfileText>
+                ))}
+              </View>
+  
+              <ProfileText>Diseases:</ProfileText>
+              <View style={styles.inlineContainer}>
+                {Object.keys(globalState.user.diseases).map((key, index) => (
+                  <ProfileText key={index}>{globalState.user.diseases[key]}</ProfileText>
+                ))}
+              </View>
+  
+              <ButtonContainer>
+                <CustomButton
+                  disabled={blockButtonSocket}
+                  onPress={() => {
+                    disableButton();
+                    socket.emit("test_broadcast", globalState.user.name);
+                    setBlockButtonSocket(false);
+                  }}
+                >
+                  <ButtonText>envio de socket</ButtonText>
+                </CustomButton>
+                <CustomButton onPress={goBack}>
+                  <ButtonText>Attack 2</ButtonText>
+                </CustomButton>
+                <CustomButton onPress={goBack}>
+                  <ButtonText>Reset</ButtonText>
+                </CustomButton>
+              </ButtonContainer>
+  
+              <SignOutButton onPress={onSignOutButtonPress}>
+                <ButtonText>Sign Out</ButtonText>
+              </SignOutButton>
+            </Container>
+          ) : (
+            <Text>Loading...</Text>
+          )}
+        </View>
+      ) : (
+        
+        <LoginModal />
+      )}
     </View>
-  ) : (
-    <Text>Loading...</Text>
-  )}
-</View>
-
   );
-};
+      }
 
 const styles = StyleSheet.create({
 
