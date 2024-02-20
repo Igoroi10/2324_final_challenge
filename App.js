960a3b2c45d32a0cc4ed771d84b27e46cc8a16b4
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2024-02-20 10:01:19 +0100

    A5T-62 [FIX] App component

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
 import { getUserData, storeUserData } from './src/helpers/asyncStorageUser';
 import { Context } from './src/helpers/Context';
 import { globalStateSchema } from './src/helpers/Constants';
-import LoginModal from './src/components/LoginModal';
+import LoginModal from './src/components/LoginComponents/Login';
 import socket from './helpers/socket';
 import styled from 'styled-components/native';
 import ProfileAcolyte from './screens/ProfileAcolyte';
-import ProfileMortimer from './screens/ProfileMortimer';
-import ProfileVillano from './screens/ProfileVillano';
-import ProfileKnight from './screens/ProfileKnight';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getAllUsers } from './src/helpers/axiosPetitions';
 
 
-const AppScreen = () => {
+const MainContainer = styled.View`
+  display: flex;
+  flex: 1;
+`
+
+const App = () => {
   const [socketEvent, setSocketEvent] = useState(null)
   const [isLogged, setIsLogged] = useState(false);
-  const [isLoginModalVisible, setLoginModalVisible] = useState(true);
   const [globalState, setGlobalState] = useState(globalStateSchema);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userRole, setUserRole] = useState('');
 
   const globalStateHandler = (data) => {
     setGlobalState(globalState => ({
@@ -31,73 +29,60 @@ const AppScreen = () => {
   useEffect(() => {
 
     const checkIfLogged = async () => {
-    const user = await AsyncStorage.getItem('user');
-    console.log(user);
-    if (user) {
-    setIsLogged(true);
-    socket.connect();
-    socket.emit("store_socket_id", user.email)
-    }
+      const user = await getUserData();
+      await getAllUsers();
+      if (user) {
+      setIsLogged(true);
+      }
     }
     checkIfLogged();
-    
+  
     },[]);
 
   useEffect(() => {
-    // Establece el rol del usuario autenticado
-    const setRole = async () => {
-   
-    //ASYNC STORAGE
-    const role = await AsyncStorage.getItem('userRole');
-    console.log('ooool', role)
-    setUserRole(role);
-    
+
+    const storeGlobalUserList = async()=>{
+
+      const userList = await getAllUsers();
+      globalStateHandler({userList:userList});
     }
-    setRole();
-    socket.connect();
-    console.log("socket conectao");
+
+    const storeGlobalUser = async()=>{
+
+      const user = await getUserData();
+      globalStateHandler({user: user});
+
+      return user;
+    }
     
-    socket.on("test_broadcast_response", (data) => {
-      console.log("*********************SOCKET TEST************************")
-      console.log(data)
-    })
 
-  },[isAuthenticated]);
+    const initialLoad = async()=>{
 
-  //Maneja el login
-  const handleLogin = async () => {
-    setIsAuthenticated(true);
-    setLoginModalVisible(false);
-  };
+      const user = await storeGlobalUser();
+      await storeGlobalUserList();
 
+      socket.connect();
+      socket.emit("store_socket_id", user.email);
+    }
+
+    if(isLogged){
+
+      initialLoad();
+    }
+  
+  },[isLogged]);
     
 
   return (
     <Context.Provider value={{ globalState, globalStateHandler }}>
-      {!isAuthenticated && <LoginModal onLogin={handleLogin} setLoginModalVisible={setLoginModalVisible} />}
-      {isAuthenticated && (
-        <>
-          {userRole === 'acolyte' && <ProfileAcolyte />}
-          {userRole === 'mortimer' && <ProfileMortimer />}
-          {userRole === 'villain' && <ProfileVillano />}
-          {userRole === 'knigth' && <ProfileKnight />}
-        </>
-      )}
+      <MainContainer>
+        {!isLogged && <LoginModal setIsLogged={setIsLogged} />}
+        {isLogged && <ProfileAcolyte/>}
+
+      </MainContainer>
+      
     </Context.Provider>
   );
 }  
 
-
-const StyledButton = styled.TouchableOpacity`
-    background-color: rgba(171, 147, 192, 0.7);
-    display: flex;
-    justify-content: center;
-    height: 60px;
-    width: 40%;
-    margin-top: 35%;
-    border-radius: 60px;
-    border: #7B26C4;
-    align-self: center;
-`;
-
-export default AppScreen;
+export default App;
