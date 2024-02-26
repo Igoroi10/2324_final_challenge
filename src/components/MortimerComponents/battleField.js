e27234729f3d7e3382d88cadf68ff8a6148d2047
soraxkairi <asier.nogueira@ikasle.aeg.eus>
2024-02-26 20:56:35 +0100

    Cuando es el turno de mortimer cura la enfermedad del acolito

--- src/components/MortimerComponents/battleField.js
+++ src/components/MortimerComponents/battleField.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import styled from 'styled-components/native';
 import { Context } from '../../helpers/Context';
-import { Image, StyleSheet, Text, View} from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import * as Progress from 'react-native-progress';
 
 
@@ -18,33 +18,33 @@ const BattleField = ({ }) => {
 
   useEffect(() => {
 
-    if(globalState.initiative.length !== 0){
-      
-        const acolites = [];
-        const knights = [];
-        const villains = [];
+    if (globalState.initiative.length !== 0) {
 
-        globalState.initiative.forEach((intiativeId)=>{
+      const acolites = [];
+      const knights = [];
+      const villains = [];
 
-        globalState.userList.forEach((userObject)=>{
+      globalState.initiative.forEach((intiativeId) => {
+
+        globalState.userList.forEach((userObject) => {
           // console.log("userlist*****************")
           // console.log(userObject.rol)
 
-          if(intiativeId === userObject._id){
+          if (intiativeId === userObject._id) {
 
-            if(userObject.rol === "acolyte"){
+            if (userObject.rol === "acolyte") {
 
               acolites.push(userObject);
-            }else if(userObject.rol !== "knight"){
+            } else if (userObject.rol !== "knight") {
               villains.push(userObject);
-            } 
-            else{
+            }
+            else {
               knights.push(userObject);
 
             }
 
           }
-        })      
+        })
       })
 
       setAcolites(acolites);
@@ -53,128 +53,171 @@ const BattleField = ({ }) => {
 
     }
 
-    
+
 
   }, [globalState.userList, globalState.initiative]);
 
   useEffect(() => {
 
-    if(globalState.currentTurn != ""){
+    if (globalState.currentTurn != "") {
       // Se recibe el email del jugador al que le toca
       const currentTurnId = globalState.currentTurn
 
       // Se muestra en pantalla a que jugador le toca
       const currentTurnPlayer = globalState.userList.filter(user => user._id === currentTurnId)
       setCurrentTurnPlayer(currentTurnPlayer[0])
-    }else{
-      
+    } else {
+
     }
-    
+
 
   }, [globalState]);
 
-  useEffect(()=>{
-    setTimeout(() => {  
-      globalStateHandler({currentMessage: ""});
+
+  const healUser = (acoliteId) => {
+    let disease = "";
+    let name;
+    globalState.userList.forEach((element) => {
+      if (element._id === acoliteId)
+      {
+        name = element.name;
+        if (element.diseases.rotting_plague === true ){
+          disease = "rotting_plague";
+        }
+        else if(element.diseases.epic_weakness === true ){
+          disease = "epic_weakness";
+
+        }
+        else if(element.diseases.marrow_apocalypse === true ){
+          disease = "marrow_apocalypse";
+
+        }
+        else if(element.diseases.ethazium === true) {
+          disease = "ethazium";
+        }
+        else{
+          const message = "Este jugador no esta enfermo";
+          globalStateHandler({currentMessage: message});
+        }
+      }
+    })
+
+    const dataToSend = {
+      id: acoliteId,
+      illness: disease,
+      active: false,
+      name: name,
+    }
+
+    socket.emit('disease_try', sendData);
+
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      globalStateHandler({ currentMessage: "" });
     }, 4000);
-  },[globalState.currentMessage])
-  
-  if(acolites === null || knights === null || villains === null){
+  }, [globalState.currentMessage])
+
+  if (acolites === null || knights === null || villains === null) {
     return null
   }
 
   return (
     <>
-    
-  <Image key={"imagen"} style={{width: '120%', height: '120%', position: 'absolute', zIndex: -1}} source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/the-final-battle-287a4.appspot.com/o/Backgrounds%2FbattleField2.png?alt=media&token=24b9bde5-c60f-454f-b5c0-8bb74345c9f4' }} />
 
-    
-    <MainContainer>
-      
-      <AcolytesContainer>
-      {
-        acolites.map((acolite, idx) => 
+      <Image key={"imagen"} style={{ width: '120%', height: '120%', position: 'absolute', zIndex: -1 }} source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/the-final-battle-287a4.appspot.com/o/Backgrounds%2FbattleField2.png?alt=media&token=24b9bde5-c60f-454f-b5c0-8bb74345c9f4' }} />
 
-          <AcolyteContainer key={acolite._id} isAlive={acolite.isAlive} >
 
-            <AcolyteImageContainer >
-              <AcolyteImage source={{ uri: acolite.imgURL }} />
-            </AcolyteImageContainer>
+      <MainContainer>
 
-            <Progress.Bar 
-              color={'rgba(100, 255, 100, 1)'}
-              style={styles.AcolyteProgressBar} 
-              progress={acolite.characterStats.hp/acolite.characterMaxStats.maxHp } 
-              
-            />
+        <AcolytesContainer>
+          {
+            acolites.map((acolite, idx) =>
 
-          </AcolyteContainer >
-        )
-      }
-      </AcolytesContainer>
-    <Spacer></Spacer>
-      <Interface>
-        <TurnContainer>
-          <Text style={{color: 'white'}}>TURNO DE: </Text>
-          <TurnImageContainer>
-            <TurnImage source={{uri: currentTurnPlayer.imgURL }} alt='currentTurn'/>
-          </TurnImageContainer>
-        </TurnContainer>
-        <MessagesContainer>
-          {globalState.currentMessage !== "" &&
-            <>
-            <InterfaceMessage>
-              <TurnImage source={{ uri: globalState.attacker.imgURL }} alt='attackerImg'/>
-              <TurnImage source={{ uri: globalState.icon.imgURL }} alt='pruebar'/>
-              <TurnImage source={{uri: globalState.defender.imgURL }} alt='defenderImg'/>
-            </InterfaceMessage>
-            <InterfaceMessage>
-              
-              <Text style={{color: 'white'}}t> {globalState.currentMessage} </Text>
-            </InterfaceMessage>
-            </>
+              <AcolyteContainer key={acolite._id} isAlive={acolite.isAlive} >
+
+                <AcolyteImageContainer >
+                  <AcolyteImage source={{ uri: acolite.imgURL }} />
+                  {globalState.user._id === globalState.currentTurn && acolite.isAlive && (
+                    <MortimerHeal onPress={healUser(acolite._id) } />
+                  )}
+                </AcolyteImageContainer>
+
+                <Progress.Bar
+                  color={'rgba(100, 255, 100, 1)'}
+                  style={styles.AcolyteProgressBar}
+                  progress={acolite.characterStats.hp / acolite.characterMaxStats.maxHp}
+
+                />
+
+              </AcolyteContainer >
+            )
           }
-        </MessagesContainer>
-      </Interface>
+        </AcolytesContainer>
+        <Spacer></Spacer>
+        <Interface>
+          <TurnContainer>
+            <Text style={{ color: 'white' }}>TURNO DE: </Text>
+            <TurnImageContainer>
+              <TurnImage source={{ uri: currentTurnPlayer.imgURL }} alt='currentTurn' />
+            </TurnImageContainer>
+          </TurnContainer>
+          <MessagesContainer>
+            {globalState.currentMessage !== "" &&
+              <>
+                <InterfaceMessage>
+                  <TurnImage source={{ uri: globalState.attacker.imgURL }} alt='attackerImg' />
+                  <TurnImage source={{ uri: globalState.icon.imgURL }} alt='pruebar' />
+                  <TurnImage source={{ uri: globalState.defender.imgURL }} alt='defenderImg' />
+                </InterfaceMessage>
+                <InterfaceMessage>
 
-      <EnemiesContainer>
-        {knights.map( knight => 
-          <EnemyContainer key={knight._id}>
+                  <Text style={{ color: 'white' }} t> {globalState.currentMessage} </Text>
+                </InterfaceMessage>
+              </>
+            }
+          </MessagesContainer>
+        </Interface>
 
-            <EnemyImageContainer>
-              <EnemyImage source={{ uri: knight.imgURL }} />
-            </EnemyImageContainer>
+        <EnemiesContainer>
+          {knights.map(knight =>
+            <EnemyContainer key={knight._id}>
 
-            <Progress.Bar 
-              color={'rgba(255, 0, 0, 1)'}
-              style={styles.KnightProgressBar} 
-              progress={knight.characterStats.hp / knight.characterMaxStats.maxHp }
-            />
+              <EnemyImageContainer>
+                <EnemyImage source={{ uri: knight.imgURL }} />
+              </EnemyImageContainer>
 
-          </EnemyContainer >
-        )}
+              <Progress.Bar
+                color={'rgba(255, 0, 0, 1)'}
+                style={styles.KnightProgressBar}
+                progress={knight.characterStats.hp / knight.characterMaxStats.maxHp}
+              />
 
-      </EnemiesContainer>
-      <EnemiesContainer>
-        {villains.map( villain => 
-          <EnemyContainer key={villain._id}>
+            </EnemyContainer >
+          )}
 
-            <EnemyImageContainer>
-              <EnemyImage source={{ uri: villain.imgURL }} />
-            </EnemyImageContainer>
+        </EnemiesContainer>
+        <EnemiesContainer>
+          {villains.map(villain =>
+            <EnemyContainer key={villain._id}>
 
-            <Progress.Bar 
-              color={'rgba(255, 0, 0, 1)'}
-              style={styles.KnightProgressBar} 
-              progress={villain.characterStats.hp / villain.characterMaxStats.maxHp }
-            />
+              <EnemyImageContainer>
+                <EnemyImage source={{ uri: villain.imgURL }} />
+              </EnemyImageContainer>
 
-          </EnemyContainer >
-        )}
+              <Progress.Bar
+                color={'rgba(255, 0, 0, 1)'}
+                style={styles.KnightProgressBar}
+                progress={villain.characterStats.hp / villain.characterMaxStats.maxHp}
+              />
 
-      </EnemiesContainer>
-    </MainContainer>
-    
+            </EnemyContainer >
+          )}
+
+        </EnemiesContainer>
+      </MainContainer>
+
     </>
   )
 }
@@ -191,9 +234,9 @@ const styles = StyleSheet.create({
     width: 60,
     height: 5,
     marginLeft: 5,
-    marginTop: 3, 
+    marginTop: 3,
   }
-  
+
 });
 
 const MainContainer = styled.View`
@@ -202,6 +245,11 @@ const MainContainer = styled.View`
   align-items:center;
   flex: 1;
 `
+
+const MortimerHeal = styled.TouchableOpacity`
+  width: 80%;
+  border-radius: 40px;
+`;
 
 const Spacer = styled.View`
   height: 40%;
@@ -289,9 +337,9 @@ const AcolyteContainer = styled.View`
   margin-left: 1%;
   align-items: center;
   justify-content: center;
-  opacity: ${(prop => prop.isAlive ? '1': '0.4')};
+  opacity: ${(prop => prop.isAlive ? '1' : '0.4')};
 `
-  
+
 const AcolyteImageContainer = styled.View`
   border: 2px solid white;
   border-radius: 100px;
