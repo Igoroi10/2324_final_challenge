da87b4672df2d6b76ebd1337381888b494c74d64
esetedos <esther.fernandez@ikasle.aeg.eus>
2024-02-27 22:54:21 +0100

    A5T-133 [UPDATE] sick modal con fondo más oscuro

--- src/components/SickModal.js
+++ src/components/SickModal.js
@@ -51,7 +51,7 @@ const ContentContainer = styled.View`
   border-radius: 0px;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
 
